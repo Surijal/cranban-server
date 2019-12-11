75af751e5eb72b084c74e790d83c91fea91988f7
Boris Vogt <boris.vogt@posteo.de>
2019-12-11 13:24:37 +0100

    model add models/project.js

---
+++ models/projects.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const projectSchema = mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    deadline: { type: Date, default: null },
+    tasks: [{ type: Schema.Types.ObjectId, ref: 'Task' }],
+    users: [{ type: Schema.Types.ObjectId, reg: 'User'}],
+    teams: [{ type: Schema.Types.ObjectId, reg: 'Team'}],
+
+}, { 
+    timestamps: { 
+    createdAt: 'created_at',
+    updatedAt: 'updated_at' 
+    }
+})
+
+
+
+const Project = mongoose.model('Project', projectSchema);
+
+module.exports = Project;
