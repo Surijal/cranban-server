6eef60385f5bb54de903d8130bf3e3eeb4d68bed
Boris Vogt <boris.vogt@posteo.de>
2019-12-11 13:28:54 +0100

    model add models/team.js

---
+++ models/teams.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const teamSchema = mongoose.Schema({
+    name: { type: String, required: true },
+    users: [{type: Schemma.Types.ObjectId, ref: 'User'}],
+    projects: [{ type: Schema.Types.ObjectId, ref: 'Project'}]
+
+}, { 
+    timestamps: { 
+    createdAt: 'created_at',
+    updatedAt: 'updated_at' 
+    }
+})
+
+
+
+const Team = mongoose.model('Team', teamSchema);
+
+module.exports = Team;
