0eb0cec861b56407be65dc13bd22e431159d5e5d
Boris Vogt <boris.vogt@posteo.de>
2019-12-11 13:22:25 +0100

    model add models/user.js

--- models/user.js
+++ models/user.js
@@ -1,15 +1,22 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+
 const userSchema = new Schema({
-  username: String,
-  password: String,
-}, {
-  timestamps: {
-    createdAt: 'created_at',
-    updatedAt: 'updated_at'
-  },
-});
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true, unique:true },
+  projects: [{ type: Schema.Types.ObjectId, ref: 'Project'}],
+  teams: [{ type: Schema.Types.ObjectId, ref: 'Team'}]
+}, { 
+timestamps: { 
+createdAt: 'created_at',
+updatedAt: 'updated_at' 
+}
+})
+
+
 
 const User = mongoose.model('User', userSchema);
 
