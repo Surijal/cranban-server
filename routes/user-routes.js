71a71f985750a28d846d2346fbb9b485d5527105
Boris Vogt <boris.vogt@posteo.de>
2019-12-11 19:43:23 +0100

    add helper function to user routes

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -6,8 +6,17 @@ const router = express.Router();
 const User = require('../models/User');
 
 
+
+// HELPER FUNCTIONS
+const {
+    isLoggedIn,
+    isNotLoggedIn,
+    validationLogin,
+    validatePassword
+} = require('../helpers/middlewares');
+
 //FIND USER BY ID
-router.get( '/:id', ( req, res, next ) => {
+router.get( '/:id', isLoggedIn, ( req, res, next ) => {
     const { id } = req.params;
 
     if ( !mongoose.Types.ObjectId.isValid(id)) {
@@ -22,6 +31,36 @@ router.get( '/:id', ( req, res, next ) => {
         .catch( (err) => {
             res.status(400).json(err)
         })
+})
+
+
+//FIND USER BY EMAIL
+router.get('/email', isLoggedIn, ( req, res, next ) => {
+    const { email } = req.body;
+
+    console.log(req.body);
+    
+
+    if ( !mongoose.Types.ObjectId.isValid(id)) {
+        res.status(500).json({message: 'Specified email is not valid'})
+        return
+    }
+
+    User.findOne(email)
+        .then( (email) => {
+            res.status(200).json(email)
+        })
+        .catch((err) => {
+            res.status(400).json(err)
+        })
+})
+
+
+
+//FIND USER AND UPDATE
+router.put('/:id', ( req, res, next ) => {
+    const { id } = req.params;
+    const { username, firstname, email, password } = req.body;
 })
 
 
