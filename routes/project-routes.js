dc6bbfb6f80db8355a59ef70a62cb1f1a3f34a12
Boris Vogt <boris.vogt@posteo.de>
2019-12-11 16:25:45 +0100

    add project get route

--- routes/project-routes.js
+++ routes/project-routes.js
@@ -4,13 +4,12 @@ const mongoose = require('mongoose');
 const router = express.Router();
 
 const Project = require('../models/projects');
+const Task = require('../models/tasks');
 
 
 //POST NEW PROJECT
 router.post('/', ( req, res, next ) => {
     const { title, description, deadline  } = req.body;
-    console.log('////////////////// POST PROJECT', req.body);
-    
 
     Project.create({ title, description, deadline, tasks: [] })
         .then( (createdProject ) => {
@@ -20,6 +19,20 @@ router.post('/', ( req, res, next ) => {
             res.status(500).json(err)
         })
 })
+
+
+//GET ALL PROJECT
+router.get('/', ( req, res, next ) => {
+    Project.find()
+        .populate('tasks')
+        .then( allProjects => {
+            res.status(200).json(allProjects)
+        })
+        .catch((err) => {
+            res.status(400).json(err)
+        })
+})
+
 
 
 
