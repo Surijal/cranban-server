8c35583e674b2632b0c18017ef54e1f33f8b461c
Boris Vogt <boris.vogt@posteo.de>
2019-12-11 13:27:06 +0100

    model add models/tasks.js

---
+++ models/tasks.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const taskSchema = mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    deadline: { type: Date, default: null },
+    project: { type: Schema.Types.ObjectID, ref: 'Project'},
+    done: { type: Boolean, default: false },
+    subTask: [{ 
+    description: { type: String, required: true }, 
+    done: { type: Boolean, default: false }
+                            }]
+
+},{ 
+    timestamps: { 
+    createdAt: 'created_at',
+    updatedAt: 'updated_at' 
+    }
+})
+
+
+
+
+
+const Task = mongoose.model('Task', taskSchema);
+
+module.exports = Task;
