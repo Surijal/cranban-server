0a6bcf95174b2ea0dea80a3b0cfb217fc38a712c
Boris Vogt <boris.vogt@posteo.de>
2019-12-11 13:30:58 +0100

    model add models/subTask.js

---
+++ models/subTasks.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const subTaskSchema = mongoose.Schema({ 
+    description: { type: String, required: true }, 
+    done: { type: Boolean, default: false }
+
+}, { 
+    timestamps: { 
+    createdAt: 'created_at',
+    updatedAt: 'updated_at' 
+    }
+})
+
+
+
+const SubTaskSchema = mongoose.model('SubTaskSchema', subTaskSchema);
+
+module.exports = SubTaskSchema;
