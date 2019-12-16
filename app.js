6c07c3025d2c876de3299986cb16190b93ecd6e1
Boris Vogt <boris.vogt@posteo.de>
2019-12-16 13:05:40 +0100

    add new url app.js

--- app.js
+++ app.js
@@ -77,6 +77,14 @@ app.use('/api/users', userRouter);
 
 
 
+// ROUTE FOR SERVING REACT APP (index.html)
+app.use((req, res, next) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+
+
 // ERROR HANDLING
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
---
+++ public/precache-manifest.32253a0a26ffcfca107a6b474abc04c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06b67bf2cc1d831c5bee7e99e2dc6f22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ea67dea892558912dcb",
+    "url": "/static/css/main.003f9b35.chunk.css"
+  },
+  {
+    "revision": "41827af23f2cce3a1c87",
+    "url": "/static/js/2.8f8df089.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/static/js/2.8f8df089.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9ea67dea892558912dcb",
+    "url": "/static/js/main.c03a27e4.chunk.js"
+  },
+  {
+    "revision": "5bba74af4a4e52b458f9",
+    "url": "/static/js/runtime-main.e8ac4ea3.js"
+  }
+]);
