5671045d51efbb8d7a1af3905bb8dc048ca454f3
Boris Vogt <boris.vogt@posteo.de>
2019-12-18 15:55:11 +0100

    add client

---
+++ public/precache-manifest.a146fc7356044a056e413fc3da91a2c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02067d5bfef9ae7a5750c47254126747",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b8001e644b337cb0653",
+    "url": "/static/js/2.4ddacc61.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/static/js/2.4ddacc61.chunk.js.LICENSE"
+  },
+  {
+    "revision": "619e8802dbc8a0513031",
+    "url": "/static/js/main.a0f9b50d.chunk.js"
+  },
+  {
+    "revision": "5bba74af4a4e52b458f9",
+    "url": "/static/js/runtime-main.e8ac4ea3.js"
+  }
+]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.32253a0a26ffcfca107a6b474abc04c6.js"
+  "/precache-manifest.a146fc7356044a056e413fc3da91a2c8.js"
 );
 
 self.addEventListener('message', (event) => {
