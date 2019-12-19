19d83110b2e0f458b3c95c9b00145952aca0f273
Boris Vogt <boris.vogt@posteo.de>
2019-12-19 12:39:36 +0100

    deploy prepare, database seed

---
+++ public/precache-manifest.5c52b7e09a0adf62bbfb83563571d90c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7dd4dc45e22ea2f021a37af1d9f2725",
+    "url": "/index.html"
+  },
+  {
+    "revision": "460b0c8db74c1a5dfb87",
+    "url": "/static/css/2.627926fe.chunk.css"
+  },
+  {
+    "revision": "48a3b2caf597262d638d",
+    "url": "/static/css/main.414ff1a1.chunk.css"
+  },
+  {
+    "revision": "460b0c8db74c1a5dfb87",
+    "url": "/static/js/2.cc953f24.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/static/js/2.cc953f24.chunk.js.LICENSE"
+  },
+  {
+    "revision": "48a3b2caf597262d638d",
+    "url": "/static/js/main.14a40daa.chunk.js"
+  },
+  {
+    "revision": "5bba74af4a4e52b458f9",
+    "url": "/static/js/runtime-main.e8ac4ea3.js"
+  }
+]);
