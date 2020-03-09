fbff00d64d608097428490cdaf9b7e415cf4c73f
Boris Vogt <boris.vogt@posteo.de>
2020-03-09 20:15:51 +0100

    add post team in routes/teams-routes.js

--- routes/team-routes.js
+++ routes/team-routes.js
@@ -4,4 +4,34 @@ const mongoose = require ("mongoose");
 
 const router = express.Router();
 
+const Team = require("../models/Teams");
+
+
+// Helpter Functions
+const {
+    isLoggedIn,
+    isNotLoggedIn,
+    validationLogin,
+    validatePassword
+} = require('../helpers/middlewares');
+
+
+// Create Team
+router.post('/teams', isLoggedIn,  async( req, res, next ) => {
+
+    const { name, description, deadline } = req.body;
+
+    const {_id} = req.session.currentUser;
+
+    try {
+        const createdTeam = await Team.create({ name, projects: [], users: [_id] });
+        await User.findByIdAndUpdate(_id, {$push: { teams: createdTeam._id}}, { new: true});
+
+        res.status(201).json(createdTeam)
+    } catch(err) {
+        res.status(400).json(err)
+    }
+})
+
+
 module.exports = router;
