43a312f86f4cf35d9960fe05ed6325a69d1ffc1f
Boris Vogt <boris.vogt@posteo.de>
2020-03-10 10:04:54 +0100

    add find team by id routes/team-routes

--- routes/team-routes.js
+++ routes/team-routes.js
@@ -48,7 +48,24 @@ router.get('/', isLoggedIn, ( req, res, next ) => {
 
 
 // Find specific Team by Team ID
+router.get('/:id', isLoggedIn, ( req, res, next ) => {
 
+    const { id } = req.params;
+
+    if ( !mongoose.Types.ObjectId.isValid( id )) {
+        
+        res.status(500).json({message: 'Specified id is not valid'})
+        return
+    }
+
+    Team.findById( id )
+        .then( foundTeam => {
+            res.status(200).json( foundTeam )
+        })
+        .catch( err => {
+            res.status(400).json(err)
+        })
+})
 
 
 module.exports = router;
